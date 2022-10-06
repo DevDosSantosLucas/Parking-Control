1d0c574e337c71d621d410053bb6d5e9d88378c4
DevDosSantosLucas <lucas1995ls@outlook.com>
2022-10-05 21:24:50 -0300

    Create Methodo PUT

--- src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
+++ src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
@@ -73,5 +73,42 @@ public class ParkingSpotController {
 
     }
 
+//    @PutMapping("/{id}")
+//    public  ResponseEntity<Object> updateParkingSpot(@PathVariable(value = "id") UUID id ,
+//                                                     @RequestBody @Valid ParkingSpotDto parkingSpotDto){
+//        Optional<ParkingSpotModel> parkingSpotModelOptional = parkingSpotService.findById(id);
+//        if(!parkingSpotModelOptional.isPresent()){
+//            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Parking spot not found.");
+//        }
+//        var parkingSpotModel = parkingSpotModelOptional.get();
+//
+//        parkingSpotModel.setParkingSpotNumber(parkingSpotDto.getParkingSpotNumber());
+//        parkingSpotModel.setLicensePlateCar(parkingSpotDto.getLicensePlateCar());
+//        parkingSpotModel.setModelCar(parkingSpotDto.getModelCar());
+//        parkingSpotModel.setBrandCar(parkingSpotDto.getBrandCar());
+//        parkingSpotModel.setColorCar(parkingSpotDto.getColorCar());
+//        parkingSpotModel.setResponsibleName(parkingSpotDto.getResponsibleName());
+//        parkingSpotModel.setApartment(parkingSpotDto.getApartment());
+//        parkingSpotModel.setBlock(parkingSpotDto.getBlock());
+//
+//        return ResponseEntity.status(HttpStatus.OK).body(parkingSpotService.save(parkingSpotModel));
+//    }
+@PutMapping("/{id}")
+public  ResponseEntity<Object> updateParkingSpot(@PathVariable(value = "id") UUID id ,
+                                                 @RequestBody @Valid ParkingSpotDto parkingSpotDto){
+    Optional<ParkingSpotModel> parkingSpotModelOptional = parkingSpotService.findById(id);
+    if(!parkingSpotModelOptional.isPresent()){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Parking spot not found.");
+    }
+
+    var parkingSpotModel = new ParkingSpotModel();
+
+    BeanUtils.copyProperties(parkingSpotDto,parkingSpotModel);
+    parkingSpotModel.setId(parkingSpotModelOptional.get().getId());
+    parkingSpotModel.setRegistrationDate(parkingSpotModelOptional.get().getRegistrationDate());
+
+    return ResponseEntity.status(HttpStatus.OK).body(parkingSpotService.save(parkingSpotModel));
+}
+
 
 }
